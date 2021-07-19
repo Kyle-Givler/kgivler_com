4538b17cacda78855ff220d510f245ccc1396a17
Kyle Givler <xvegan88x@gmail.com>
2021-07-19 15:45:28 -0400

    Add FizzBuzz

---
+++ FizzBuzz/js/site.js
@@ -0,0 +1,59 @@
+function getInput() {
+    let fizz = document.getElementById("fizz").value;
+    let buzz = document.getElementById("buzz").value;
+
+    fizz = parseInt(fizz);
+    buzz = parseInt(buzz);
+
+    if(!Number.isInteger(fizz) || !Number.isInteger(buzz)) {
+        alert("You must enter integers!");
+        return;
+    }
+
+    let results = generateResult(fizz, buzz, 100)
+    displayResults(results);
+}
+
+function generateResult(fizz, buzz, max) {
+    let output = [];
+
+    for(let i = 1; i <= max; i++) {
+        if(i % fizz == 0 && i % buzz == 0) {
+            output.push("FizzBuzz");
+        } else if (i % fizz == 0) {
+            output.push("Fizz");
+        } else if (i % buzz == 0) {
+            output.push("Buzz");
+        } else {
+            output.push(i);
+        }
+    }
+
+    return output;
+}
+
+function displayResults(results) {
+    let templateRows = "";
+
+    for(let i = 0; i < results.length; i++) {
+        let className = "";
+
+        if(!Number.isInteger(results[i])) {
+            className = ` class="${results[i]}"`;
+        }
+
+        if(i == 0 || templateRows.endsWith("</tr>"))
+        {
+            templateRows += "<tr>"
+        }
+
+        templateRows += `<td${className}>${results[i]}</td>`;
+
+        if((i + 1) % 5 == 0)
+        {
+            templateRows += "</tr>"
+        }
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
+}
